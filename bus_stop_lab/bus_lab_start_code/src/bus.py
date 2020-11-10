2c932f174917a5a29e5a40bdc0dc4aaac7c09c44
saerx <saerlaith.robyn@gmail.com>
2020-11-10 16:13:49 +0000

    first commit bus and person classes added

---
+++ bus_stop_lab/bus_lab_start_code/src/bus.py
@@ -0,0 +1,7 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+
+    def drive():
+        return "Brum brum"
