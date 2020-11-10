ce592e5a1dc300a5f0e349d8561eec8644593b1e
saerx <saerlaith.robyn@gmail.com>
2020-11-10 17:16:11 +0000

    added pick up from stop method to bus class

--- bus_stop_lab/bus_lab_start_code/src/bus.py
+++ bus_stop_lab/bus_lab_start_code/src/bus.py
@@ -18,3 +18,8 @@ class Bus:
 
     def empty(self):
         self.passengers.clear()
+
+    def pick_up_from_stop(self, bus_stop):
+        new_passengers = bus_stop.queue
+        for passenger in new_passengers:
+            self.passengers.append(passenger)
