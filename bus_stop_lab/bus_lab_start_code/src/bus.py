8c96fba7f257061988b1dc17276aee3a99ce504c
saerx <saerlaith.robyn@gmail.com>
2020-11-10 16:33:18 +0000

    added passenger_count function

--- bus_stop_lab/bus_lab_start_code/src/bus.py
+++ bus_stop_lab/bus_lab_start_code/src/bus.py
@@ -2,6 +2,10 @@ class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
+        self.passengers = []
 
     def drive(self):
         return "Brum brum"
+    
+    def passenger_count(self):
+        return len(self.passengers)
